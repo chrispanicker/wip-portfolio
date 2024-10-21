a576c0e91c978aabe3a226548a61d6b106d09cc4
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2024-10-20 23:47:16 -0400

    font sizing

--- wip-portfolio/src/app/page.tsx
+++ wip-portfolio/src/app/page.tsx
@@ -4,11 +4,14 @@ export default function Home() {
   const gridParent = "flex lg:flex-row flex-col mono-ital uppercase lg:pb-0 pb-4"
 
   return (
-    <main className="w-screen h-screen border border-[1rem] border-black transition-[transform] min-[1300px]:rotate-[-2deg] min-[1000px]:rotate-[2deg] min-[800px]:rotate-[-2deg] rotate-[2deg] bg-white overflow-y-scroll">
-      <header className="lg:h-[10%] h-[6%] bg-white lg:text-[6rem] lg:tracking-[-.3rem] min-[500px]:text-[3rem] tracking-[-.2rem] flex justify-start items-center  font-bold lg:mx-5 mx-2"><h1>Panicker</h1></header>
-      <div className="lg:h-[7%] h-[4%] bg-black text-white lg:text-[2.6rem] min-[500px]:text-[1.5rem] flex justify-start items-center lg:px-5 px-2"><h2 className="font-bold">Work Permit&nbsp;</h2><h2>Department of Portfolios</h2></div>
+    <main className="w-screen h-screen border border-[1rem] outline outline-black outline-2 border-black transition-[transform] min-[1300px]:rotate-[-2deg] min-[1000px]:rotate-[2deg] min-[800px]:rotate-[-2deg] rotate-[2deg] bg-white overflow-y-scroll">
+      <header className="lg:h-[10%] bg-white lg:text-[6rem] lg:tracking-[-.3rem] text-[3rem] tracking-[-.2rem] flex justify-start items-center  font-bold lg:mx-5 mx-2"><h1>Panicker</h1></header>
+      <div className="lg:h-[7%] lg:py-0 py-2 bg-black text-white lg:text-[2.6rem] min-[500px]:text-[1.5rem] flex lg:flex-row flex-col justify-center lg:justify-start items-start leading-[1.7rem] lg:items-center lg:px-5 px-2">
+        <h2 className="font-bold">Work Permit&nbsp;</h2>
+        <h2>Department of Portfolios</h2>
+      </div>
       
-      <section className="h-[53%] lg:grid lg:grid-cols-2 lg:p-5 p-2 lg:text-[1.5rem]">
+      <section className="lg:h-[53%] lg:grid lg:grid-cols-2 lg:p-5 p-2 lg:text-[1.5rem]">
           <div className="lg:grid lg:grid-rows-2">
             <span className={gridParent}><p className={`${category}`}>Permit Number:&nbsp;</p><p>4815162342-08-CP</p></span>
             <span className={gridParent}><p className={`${category}`}>Address:&nbsp;</p><p>23 URMAS PL, NEW YORK, NY 10011 </p></span>
@@ -25,7 +28,7 @@ export default function Home() {
           </div>
 
       </section>
-      <footer className={`h-[37%] lg:h-[30%] lg:p-5 p-2 border-t-[1rem] border-black lg:text-[1.5rem] mono-ital flex justify-between flex-col`}>
+      <footer className={`lg:h-[30%] lg:p-5 p-2 border-t-[1rem] border-black lg:text-[1.5rem] mono-ital flex justify-between flex-col`}>
         <div className="">
           <p className="pb-2 uppercase">Chris is currently the designer for Pitchfork.</p>
           <p className="pb-2 uppercase">His work is featured on pitchfork and gq, on <a className="hover:underline" href="https://www.instagram.com/p/C92zCcASIs0/?img_index=1">Charli xcx&apos;s Instagram</a></p>
