0ad3aadeecad58f5a0d864b32ae84dccb52256db
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2024-10-21 00:41:45 -0400

    boldening

--- wip-portfolio/src/app/page.tsx
+++ wip-portfolio/src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <main className="w-screen h-screen border border-[1rem] outline outline-black outline-2 border-black transition-[transform] min-[1300px]:rotate-[-2deg] min-[1000px]:rotate-[2deg] min-[800px]:rotate-[-2deg] rotate-[2deg] bg-white overflow-y-scroll">
       <header className="lg:h-[10%] bg-white lg:text-[6rem] text-[5rem] flex justify-start items-center lg:leading-auto leading-[5.2rem] lg:mx-5 mx-2 sans-bold"><h1>Panicker</h1></header>
-      <div className="lg:h-[7%] lg:py-0 py-2 bg-black text-white sans lg:text-[2.6rem] min-[500px]:text-[2rem] flex items-center lg:px-5 px-2">
-        <h2 className="inline sans-bold">Work Permit</h2>
-        <h2 className="inline">&nbsp;Department of Portfolios</h2>
+      <div className="lg:h-[7%] lg:py-0 py-2 bg-black text-white sans lg:text-[2.6rem] text-[2rem] flex lg:flex-row flex-col lg:items-center lg:px-5 px-2 lg:leading-[1.5rem] leading-[2.3rem]">
+        <h2 className="inline sans-bold lg:pr-4">Work Permit</h2>
+        <h2 className="inline">Department of Portfolios</h2>
       </div>
       
       <section className="lg:grid lg:grid-cols-2 lg:p-5 p-2 lg:text-[1.5rem]">
@@ -29,10 +29,9 @@ export default function Home() {
           </div>
 
       </section>
-      <footer className={`lg:h-[30%] lg:p-5 p-2 pb-32 border-t-[1rem] border-black lg:text-[1.5rem] mono-ital flex justify-between flex-col`}>
-        <div className="sans">
-          <p className="pb-2">Chris is currently the designer for Pitchfork.</p>
-          <p className="pb-2">His work is featured on Pitchfork and GQ, and on <a className="hover:underline" href="https://www.instagram.com/p/C92zCcASIs0/?img_index=1">Charli xcx&apos;s Instagram.</a></p>
+      <footer className={`lg:h-[30%] lg:p-5 p-2 pb-32 border-t-[1rem] border-black text-[1.5rem] mono-ital flex justify-between flex-col`}>
+        <div className="sans pb-8">
+          <p className="pb-2">Chris is currently the designer for Pitchfork. His work is featured on Pitchfork and GQ, and on <a className="hover:underline" href="https://www.instagram.com/p/C92zCcASIs0/?img_index=1">Charli xcx&apos;s Instagram.</a></p>
           <p className="pb-2">He is open for work, and for any inquries hit him at <a className="hover:underline" href="mailto:chris@panicker.design">chris@panicker.design</a></p>
         </div>
         <p className="sans">This portfolio is slated to be completed by September 18, 20XX.* In the meantime, enjoy your time here.  </p>
