8bed0329a11bd3c79ccb13c01929f3db10eb671e
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2025-01-02 22:27:49 -0500

    safari sizing

--- wip-portfolio/src/app/page.tsx
+++ wip-portfolio/src/app/page.tsx
@@ -8,9 +8,9 @@ const allProjects = await getProjects()
 //test
 export default function Home() {
   return (<>
-      <main className="w-screen h-screen absolute top-0 left-0 lg:p-10 p-5 mono-ital">
+      <main className="w-screen h-[100dvh] absolute top-0 left-0 lg:p-10 p-5 mono-ital">
         <Suspense>
-          <SynchronizedScrollComponent allProjects={allProjects} />
+            <SynchronizedScrollComponent allProjects={allProjects} />
         </Suspense>
         <footer className=" w-full flex justify-between items-end h-[5%]">
           <button className="sans hover:underline">CHRIS PANICKER</button>
