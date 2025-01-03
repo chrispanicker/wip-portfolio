b0302dcf2995d67977dc51a865c8d37186c6dd44
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2025-01-02 23:22:16 -0500

    update

--- wip-portfolio/src/app/page.tsx
+++ wip-portfolio/src/app/page.tsx
@@ -5,7 +5,7 @@ const allProjects = await getProjects()
 
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
-//test
+
 export default function Home() {
   return (<>
       <main className="w-screen h-[100svh] absolute top-0 left-0 lg:p-10 p-5 mono ">
